ecb375f306410fd4119de2e7297f245ed59488c8
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-10-01 22:11:25 -0500

    Shows posts on front-end

--- client/src/components/posts.jsx
+++ client/src/components/posts.jsx
@@ -17,6 +17,9 @@ function Posts() {
 
     return (
         <div>
+            {posts.map((post) => (
+                <div key={post.id}>{post.title}</div>
+            ))}
         </div>
     )
 }
