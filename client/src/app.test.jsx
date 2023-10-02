787c035a46de01a1d618847ba77466cd80307def
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-10-02 15:03:29 -0500

    Adds test for blog title

--- client/src/app.test.jsx
+++ client/src/app.test.jsx
@@ -1,3 +1,9 @@
 import { describe, expect, test } from 'vitest';
 import { render } from '@testing-library/react';
 import App from './App.jsx';
+
+test("say hello to Techtonica", () => {
+    const { getByText } = render(<App />);
+    expect(getByText("Diaries Online")).toBeDefined();
+
+});
