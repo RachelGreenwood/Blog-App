a949165761afd84cc822afbe8135f332959fb57d
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-10-01 22:08:51 -0500

    Adds GET request to server

--- client/src/components/posts.jsx
+++ client/src/components/posts.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from 'react';
 function Posts() {
+    const [posts, setPosts] = useState([]);
+
+    const getRequest = () => {
+        fetch("http://localhost:8080/posts")
+            .then((response) => response.json())
+            .then((posts) => {
+                setPosts(posts);
+                console.log('Posts fetched...', posts);
+            });
+    }
+  
+    useEffect(() => {
+        getRequest();
+    }, []);
+
     return (
-        <div></div>
+        <div>
+        </div>
     )
 }
 
